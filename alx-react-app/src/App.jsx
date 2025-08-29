f943d4f70d3addb8503abb7851c57a7ae6296af4
jonathan <ishimwejonathan0@gmail.com>
2025-08-29 11:11:13 +0200

    Add Userprofile component using props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,12 +1,21 @@
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
+
 
 function App() {
   return (
     <div>
       <Header />
       <MainContent />
+
+        <UserProfile 
+        name="Jonathan Ishimwe" 
+        age="25" 
+        bio="Enjoys coding and exploring new cities" 
+      />
+
       <Footer />
     </div>
   );
