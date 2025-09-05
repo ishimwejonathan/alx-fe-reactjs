9035b9922e8daefcd18706d88450e5cfbdaebf56
jonathan <ishimwejonathan0@gmail.com>
2025-09-05 16:37:42 +0200

    made change on header

--- alx-react-app/src/components/Header.jsx
+++ alx-react-app/src/components/Header.jsx
@@ -1,9 +1,16 @@
 function Header() {
-    return (
-        <header>
-            <h1>My Favorite Cities</h1>
-        </header>
-    );
+  return (
+    <header
+      style={{
+        backgroundColor: "lightblue",
+        color: "darkblue",
+        textAlign: "center",
+        padding: "20px"
+      }}
+    >
+      <h1>My Favorite Cities</h1>
+    </header>
+  );
 }
 
 export default Header;
