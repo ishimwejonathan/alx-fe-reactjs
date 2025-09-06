4d25edfdb70620bb02534e1b2669a993aadef2ce
jonathan <ishimwejonathan0@gmail.com>
2025-09-06 13:00:41 +0200

    userprofile.jsx

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
-import UserContext from "../UserContext";
+import UserContext from "../UserContext";  // adjust the path if needed
 
-const UserProfile = () => {
+function UserProfile() {
   const userData = useContext(UserContext);
 
   return (
@@ -11,6 +11,6 @@ const UserProfile = () => {
       <p>Bio: {userData.bio}</p>
     </div>
   );
-};
+}
 
 export default UserProfile;
