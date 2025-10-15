360002566da53e71dddc016e6bb92bb3126b2780
jonathan <ishimwejonathan0@gmail.com>
2025-10-15 17:17:05 +0200

    made an update

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,20 +1,9 @@
-import React, { useState } from "react";
-import RegistrationForm from "./components/RegistrationForm";
-import FormikForm from "./components/formikForm";
+import TodoList from "./components/TodoList";
 
 function App() {
-  const [useFormik, setUseFormik] = useState(false);
-
   return (
-    <div className="p-6">
-      <button
-        onClick={() => setUseFormik(!useFormik)}
-        className="mb-4 bg-blue-500 text-white px-4 py-2 rounded"
-      >
-        Switch to {useFormik ? "Controlled Form" : "Formik Form"}
-      </button>
-
-      {useFormik ? <FormikForm /> : <RegistrationForm />}
+    <div className="App">
+      <TodoList />
     </div>
   );
 }
