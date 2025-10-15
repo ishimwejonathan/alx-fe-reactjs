11b545a3281968eadae6c0fd192603ea6101045f
jonathan <ishimwejonathan0@gmail.com>
2025-10-15 11:49:37 +0200

    fixed some errors

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,9 +1,20 @@
-import TodoList from "./components/TodoList";
+import React, { useState } from "react";
+import RegistrationForm from "./components/RegistrationForm";
+import FormikForm from "./components/formikForm";
 
 function App() {
+  const [useFormik, setUseFormik] = useState(false);
+
   return (
-    <div className="App">
-      <TodoList />
+    <div className="p-6">
+      <button
+        onClick={() => setUseFormik(!useFormik)}
+        className="mb-4 bg-blue-500 text-white px-4 py-2 rounded"
+      >
+        Switch to {useFormik ? "Controlled Form" : "Formik Form"}
+      </button>
+
+      {useFormik ? <FormikForm /> : <RegistrationForm />}
     </div>
   );
 }
